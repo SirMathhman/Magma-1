e7ca667b7ef415805aed48b7df2dcf1468763f47
SirMathhman <sirmathhman@gmail.com>
2020-06-14 21:45:44 -0400

    Add Test

--- assert.h
+++ assert.h
@@ -5,4 +5,14 @@
 #ifndef MAGMA_1_ASSERT_H
 #define MAGMA_1_ASSERT_H
 
+#include "lang.h"
+
+typedef struct Test {
+    string name;
+
+    void (*run)();
+} Test;
+
+Test Test_(string name, void(*run)());
+
 #endif //MAGMA_1_ASSERT_H
